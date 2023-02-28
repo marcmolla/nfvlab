1dcfa9307a0f8027e532b7cc26a77ca38af4f3a2
marcmolla <marcmolla@gmail.com>
2023-02-28 19:37:28 +0100

    updated with lxd reinstallation

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # NFV laboratory session 12 and 13
-This repository contains the basic scripts and skeletons for the NFV laboratory seasson 12 and 13.
+This repository contains the basic scripts and skeletons for the NFV laboratory seasson 12 and 13. 
+## Installation
+For installing the lab environment, please execute:
+```bash
+student@uk8s:~/nfvlab$ sudo ./bootsrap/bootstrap-nfvlab12-13.bash
+```
+After the installation, please check that your environment has the new groups:
+```bash
+student@uk8s:~/nfvlab$ newgrp microk8s
+student@uk8s:~/nfvlab$ newgrp lxd
+```
--- bootstrap/bootstrap-nfvlab12-13.bash
+++ bootstrap/bootstrap-nfvlab12-13.bash
@@ -3,12 +3,16 @@
 [ -z "$SUDO_USER" ] && exit 1
 K8S_VERSION="1.25-strict/stable"
 JUJU_VERSION="3.0/stable"
+LXD_VERSION="5.11/stable"
 apt update && apt upgrade -y
-for f in $(snap list | egrep '(microk8s|juju|charmcraft)' | awk '{print $1}')
+for f in $(snap list | egrep '(microk8s|juju|charmcraft|lxd)' | awk '{print $1}')
 do
   echo "Removing installed $f snap"
   snap remove $f --purge
 done
+
+echo '===== Installing LXD ====='
+snap install lxd --channel $LXD_VERSION
 
 echo "===== Installing microk8s ====="
 snap install microk8s --channel $K8S_VERSION
@@ -27,8 +31,6 @@ chown $SUDO_UID:$SUDO_GID $CONFIG_YAML
 
 echo "===== Installing juju ====="
 snap install juju --channel $JUJU_VERSION
-mkdir -p $HOME/.local/share
-chown -R $SUDO_USER:$SUDO_USER $HOME/.local
 
 echo "===== Installing charmcraft ====="
 snap install charmcraft --classic
@@ -67,7 +69,9 @@ projects: []
 cluster: null
 EOF
 usermod -a -G lxd student
-echo "***********************************************"
-echo "* Please type newgrp microk8s and newgrp lxd  *"
-echo "* or reboot the VM and enter again as student *"
-echo "***********************************************"
+mkdir -p $HOME/.local/share
+chown -R $SUDO_USER:$SUDO_USER $HOME/.local
+echo "****************************************************"
+echo "* Please type newgrp snap_microk8s and newgrp lxd  *"
+echo "*  or reboot the VM and enter again as student      *"
+echo "****************************************************"
